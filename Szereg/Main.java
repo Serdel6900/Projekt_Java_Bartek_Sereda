22c5147324bdfe74fc92d70a9f967d317d241125
Serdel6900 <bartek.sereda.bs@gmail.com>
2025-06-14 16:34:17 +0200

    Add files via upload

---
+++ Szereg/Main.java
@@ -0,0 +1,8 @@
+package Szereg;
+
+public  class Main {
+    public static void main(String[] args) {
+        TaskSchedulerApp  taskSchedulerApp = new TaskSchedulerApp();
+        taskSchedulerApp.start();
+    }
+}
